b6e9b26e61f68a989b07124022b0d92199762dc5
emmanuel gd <emma_nuel011@hotmail.com>
2020-04-07 08:30:18 -0500

    hola

---
+++ HolaMundo/HolaMundo.cpp
@@ -0,0 +1,39 @@
+// HolaMundo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    //Antes de usar una variable tengo que declararla
+    int x;
+    int y;
+    int a;
+    int b;
+    int c;
+
+    //Despues de declarar la variable ya puedo usarla
+    std::cout << "Ingresa el valor de x: ";
+    std::cin >> x;
+    std::cout << "Ingresa el valor de y: "; 
+    std::cin >> y;
+    a = x + y;
+    b = x - y;
+    c = x * y;
+
+    std::cout << "El resultado de x + y es: " << a << std::endl;
+    std::cout << "El resultado de x - y es: " << b << std::endl;
+    std::cout << "El resultado de x multiplicado por y es: " << c << std::endl;
+
+    return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
