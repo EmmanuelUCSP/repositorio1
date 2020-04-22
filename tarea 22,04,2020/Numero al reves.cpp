ecfdac2706b11724977f0e2a291f706a66de17cf
emmanuel gd <emma_nuel011@hotmail.com>
2020-04-22 13:40:53 -0500

    Tarea

---
+++ tarea 22,04,2020/Numero al reves.cpp
@@ -0,0 +1,26 @@
+#incluir  <iostream>
+
+usando namespace std;
+
+int main() {
+
+    int numero - 0, contador, 0, a, 0
+};
+cout << "Enter un numero: n";
+cin >> numero;
+int dmodificar - numero;
+mientras(pmodificar > 0)
+{
+    dmodificar / o 10;
+    contador++;
+}
+//cout << contador<<endl;
+//Invertira el orden
+para(contador; > 0; contador--)
+a - numero % 10;
+cout << a << "  ";
+numero / 10;
+    }
+    cout << endl;
+    retorno 0;
+}
